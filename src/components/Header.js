8bbd859b08228b0097ecb61a5ab1000008af6678
marinakim44 <marina.kim.mk90@gmail.com>
2024-03-23 20:44:58 +0000

    redeploy before pwa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import "../containers/App.css";
 import { PureComponent } from "react";
-import CounterButton from "./CounterButton";
 
 class Header extends PureComponent {
   render() {
-    console.log("header component");
     return (
       <div>
-        <CounterButton color={"red"} />
         <h1 className="f1">ROBOTBOOK</h1>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
